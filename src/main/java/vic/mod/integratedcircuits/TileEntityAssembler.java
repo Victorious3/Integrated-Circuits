25a3261f86a45eed756ccccdb2350fb0d4753388
Victorious3 <Minefoto0@gmail.com>
2014-09-07 18:16:59 +0200

    Framebuffer. Still malfunctioning.

--- src/main/java/vic/mod/integratedcircuits/TileEntityAssembler.java
+++ src/main/java/vic/mod/integratedcircuits/TileEntityAssembler.java
@@ -22,25 +22,38 @@ public class TileEntityAssembler extends TileEntity
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
 		GL11.glViewport(0, 0, 64, 64);
-//		GL11.glOrtho(0, 64, 64, 0, -1, 1); If I enable that, it won't draw anything at all.
+		GL11.glOrtho(0, 64, 64, 0, -1, 1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
 		
-		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
+        GL11.glDisable(GL11.GL_BLEND);
+		
 		GL11.glColor3f(1F, 0, 0);
 		GL11.glBegin(GL11.GL_QUADS);
 		GL11.glVertex2i(0, 0);
-		GL11.glVertex2i(0, 64);
-		GL11.glVertex2i(64, 64);
-		GL11.glVertex2i(64, 0);
+		GL11.glVertex2i(0, 45);
+		GL11.glVertex2i(45, 45);
+		GL11.glVertex2i(45, 0);
 		GL11.glEnd();
-		GL11.glColor3f(0, 1F, 0);
-		GL11.glBegin(GL11.GL_POINTS);
-		GL11.glVertex2i(0, 0);
+		
+		GL11.glLineWidth(1F);
+		GL11.glColor3f(1F, 1F, 0);
+		GL11.glBegin(GL11.GL_LINES);
+		GL11.glVertex2i(1, 1);
+		GL11.glVertex2i(15, 30);
 		GL11.glEnd();
+		
 		GL11.glBegin(GL11.GL_POINTS);
 		GL11.glVertex2i(25, 25);
 		GL11.glEnd();
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glBegin(GL11.GL_POINTS);
+		GL11.glVertex2i(63, 63);
+		GL11.glEnd();
+		
 		circuitFBO.unbindFramebuffer();
 	}
 }
