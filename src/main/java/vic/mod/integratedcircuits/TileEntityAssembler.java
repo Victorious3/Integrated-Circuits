35ec61a555b94f38a828f1d0f33717f53fd727b1
Victorious3 <Minefoto0@gmail.com>
2014-09-07 14:31:38 +0200

    Some more progress, unfinished frame buffer.

--- src/main/java/vic/mod/integratedcircuits/TileEntityAssembler.java
+++ src/main/java/vic/mod/integratedcircuits/TileEntityAssembler.java
@@ -1,8 +1,46 @@
 package vic.mod.integratedcircuits;
 
+import net.minecraft.client.shader.Framebuffer;
 import net.minecraft.tileentity.TileEntity;
+
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class TileEntityAssembler extends TileEntity
 {
-
+	@SideOnly(Side.CLIENT)
+	public Framebuffer circuitFBO;
+	
+	@SideOnly(Side.CLIENT)
+	public void initFramebuffer()
+	{
+		circuitFBO = new Framebuffer(64, 64, false);
+		circuitFBO.bindFramebuffer(false);
+		
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glViewport(0, 0, 64, 64);
+//		GL11.glOrtho(0, 64, 64, 0, -1, 1); If I enable that, it won't draw anything at all.
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL11.glColor3f(1F, 0, 0);
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2i(0, 0);
+		GL11.glVertex2i(0, 64);
+		GL11.glVertex2i(64, 64);
+		GL11.glVertex2i(64, 0);
+		GL11.glEnd();
+		GL11.glColor3f(0, 1F, 0);
+		GL11.glBegin(GL11.GL_POINTS);
+		GL11.glVertex2i(0, 0);
+		GL11.glEnd();
+		GL11.glBegin(GL11.GL_POINTS);
+		GL11.glVertex2i(25, 25);
+		GL11.glEnd();
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		circuitFBO.unbindFramebuffer();
+	}
 }
--- src/main/java/vic/mod/integratedcircuits/client/TileEntityAssemblerRenderer.java
+++ src/main/java/vic/mod/integratedcircuits/client/TileEntityAssemblerRenderer.java
@@ -1,14 +1,57 @@
 package vic.mod.integratedcircuits.client;
 
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
+
+import vic.mod.integratedcircuits.IntegratedCircuits;
 import vic.mod.integratedcircuits.TileEntityAssembler;
 
 public class TileEntityAssemblerRenderer extends TileEntitySpecialRenderer
 {
+	ResourceLocation safetyRegulationsTex = new ResourceLocation(IntegratedCircuits.modID, "textures/blocks/assembler_safety.png");
+	ResourceLocation bottomTex = new ResourceLocation(IntegratedCircuits.modID, "textures/blocks/assembler_bottom.png");
+	
 	public void renderTileEntityAt(TileEntityAssembler te, double x, double y, double z, float partialTicks)
-	{
+	{	
+		GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glPushMatrix();
+		GL11.glTranslated(x, y, z);
 		
+		Tessellator tes = Tessellator.instance;
+		this.bindTexture(bottomTex);
+		tes.startDrawingQuads();
+		tes.addVertexWithUV(0, 8 / 16F, 0, 0, 0);
+		tes.addVertexWithUV(0, 8 / 16F, 1, 0, 1);
+		tes.addVertexWithUV(1, 8 / 16F, 1, 1, 1);
+		tes.addVertexWithUV(1, 8 / 16F, 0, 1, 0);
+		tes.draw();
+		
+		if(te.circuitFBO == null) te.initFramebuffer();
+		
+		te.circuitFBO.bindFramebufferTexture();
+		tes.startDrawingQuads();
+		tes.addVertexWithUV(0, 9 / 16F, 0, 0, 0);
+		tes.addVertexWithUV(0, 9 / 16F, 1, 0, 1);
+		tes.addVertexWithUV(1, 9 / 16F, 1, 1, 1);
+		tes.addVertexWithUV(1, 9 / 16F, 0, 1, 0);
+		tes.draw();
+
+		GL11.glRotatef(180, 0, 0, 1);
+		GL11.glTranslatef(-1.005F, -1, 0);
+		
+		this.bindTexture(safetyRegulationsTex);
+		tes.startDrawingQuads();
+		tes.addVertexWithUV(0, 9 / 16F, 1 - 7 / 16F, 0, 0);
+		tes.addVertexWithUV(0, 1 - 3 / 16F, 1 - 7 / 16F, 0, 1);
+		tes.addVertexWithUV(0, 1 - 3 / 16F, 0 + 1 / 16F, 1, 1);
+		tes.addVertexWithUV(0, 9 / 16F, 0 + 1 / 16F, 1, 0);
+		tes.draw();
+		
+		GL11.glPopMatrix();
 	}
 	
 	@Override
