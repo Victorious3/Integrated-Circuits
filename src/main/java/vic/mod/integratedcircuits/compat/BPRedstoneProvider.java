e85fc83c533eb94b47fd2dc98cd2a511519b9707
Victorious3 <Minefoto0@gmail.com>
2015-03-23 07:10:44 +0100

    And that.

---
+++ src/main/java/vic/mod/integratedcircuits/compat/BPRedstoneProvider.java
@@ -0,0 +1,36 @@
+package vic.mod.integratedcircuits.compat;
+
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+import vic.mod.integratedcircuits.gate.GateProvider;
+import vic.mod.integratedcircuits.gate.PartGate;
+import codechicken.lib.vec.BlockCoord;
+
+import com.bluepowermod.api.BPApi;
+import com.bluepowermod.api.wire.redstone.IBundledDevice;
+import com.bluepowermod.api.wire.redstone.IBundledDeviceWrapper;
+import com.bluepowermod.api.wire.redstone.IRedstoneDevice;
+import com.bluepowermod.api.wire.redstone.IRedstoneProvider;
+
+public class BPRedstoneProvider implements IRedstoneProvider
+{
+	public BPRedstoneProvider()
+	{
+		BPApi.getInstance().getRedstoneApi().registerRedstoneProvider(this);
+	}
+	
+	@Override
+	public IRedstoneDevice getRedstoneDeviceAt(World world, int x, int y, int z, ForgeDirection side, ForgeDirection face)
+	{
+		return (IRedstoneDevice)getBundledDeviceAt(world, x, y, z, side, face);
+	}
+	
+	@Override
+	public IBundledDevice getBundledDeviceAt(World world, int x, int y, int z, ForgeDirection side, ForgeDirection face)
+	{
+		PartGate gate = GateProvider.getGateAt(world, new BlockCoord(x, y, z), side.ordinal());
+		if(gate != null && gate.getProvider() instanceof IBundledDeviceWrapper) 
+			return ((IBundledDeviceWrapper)gate.getProvider()).getBundledDeviceOnSide(face);
+		return null;
+	}
+}
