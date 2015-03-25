7a824e64899a06168fff31be7aac14804f5f5ddf
Skye M <skyem@hotmail.co.uk>
2015-03-25 21:06:57 +0000

    Added message for people who don't know where to right click with the floppy. I called it "idiot" because I am an idiot because I didn't get this.

--- src/main/java/vic/mod/integratedcircuits/item/ItemFloppyDisk.java
+++ src/main/java/vic/mod/integratedcircuits/item/ItemFloppyDisk.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
+import net.minecraft.util.StatCollector;
 
 public class ItemFloppyDisk extends ItemBase
 {
@@ -27,6 +28,11 @@ public class ItemFloppyDisk extends ItemBase
 			itemInformation.add(ChatFormatting.GRAY + "Name: " + ChatFormatting.WHITE + comp.getCompoundTag("properties").getString("name"));
 			itemInformation.add(ChatFormatting.GRAY + "Size: " + ChatFormatting.WHITE + size + "x" + size);
 			itemInformation.add(ChatFormatting.GRAY + "Author: " + ChatFormatting.WHITE + comp.getCompoundTag("properties").getString("author"));
+			/*itemInformation.add(ChatFormatting.WHITE + "" + ChatFormatting.ITALIC + StatCollector.translateToLocalFormatted(getUnlocalizedName() + ".tooltip.idiot.line1"));
+			itemInformation.add(ChatFormatting.WHITE + "" + ChatFormatting.ITALIC + StatCollector.translateToLocalFormatted(getUnlocalizedName() + ".tooltip.idiot.line2"));*/
+		} else {
+			itemInformation.add(ChatFormatting.GRAY + "" + ChatFormatting.ITALIC + StatCollector.translateToLocalFormatted(getUnlocalizedName() + ".tooltip.idiot.line1"));
+			itemInformation.add(ChatFormatting.GRAY + "" + ChatFormatting.ITALIC + StatCollector.translateToLocalFormatted(getUnlocalizedName() + ".tooltip.idiot.line2"));
 		}
 	}
 }
