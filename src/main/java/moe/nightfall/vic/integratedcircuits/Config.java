721b2632b2fa3da299a4a811d7bb06c3cf5c7370
Skye M <skyem@hotmail.co.uk>
2015-04-11 17:22:58 +0100

    Made the number of undos configurable.

--- src/main/java/moe/nightfall/vic/integratedcircuits/Config.java
+++ src/main/java/moe/nightfall/vic/integratedcircuits/Config.java
@@ -15,6 +15,7 @@ public class Config
 	public static boolean showStartupMessage;
 	public static boolean enablePropertyEdit;
 	public static boolean enableTracker;
+	public static int circuitCacheSize;
 	
 	//TODO Config options for enabling/disabling gates.
 	//TODO Generalize!
@@ -22,16 +23,18 @@ public class Config
 	{
 		config = new Configuration(file);
 		config.load();
-		
-		showConfirmMessage = config.get("GENERAL", "showConfirmMessage", true, "Show the CAD's confirm promt.");
+
+		// General Configuration
+		showConfirmMessage = config.get("GENERAL", "showConfirmMessage", true, "Show the CAD's confirm prompt.");
 		showStartupMessage = config.getBoolean("showStartupMessage", "GENERAL", true, "Show a message on startup warning your players from the risk they are undertaking by playing with this mod.");
 		enablePropertyEdit = config.getBoolean("enablePropertyEdit", "GENERAL", true, "Enable property editing for the circuit peripheral. I don't take any warranty for crashes that might arise because of this.");
-		enableTracker = config.getBoolean("enableTracker", "GENERAL", true, 
-			"This setting will make the game visit this URL on startup: https://raw.githubusercontent.com/Victorious3/Integrated-Circuits/master/version.dat. " + 
-			"The connection is established over bit.ly for statistics. The data collected is publicly visible on https://bitly.com/1GIaUA6+. Bit.ly will track " +
-			"your country via your IP address, no other, or personal, information is gathered. I like statistics, and I would like you to keep this setting enabled, " +
+		circuitCacheSize = config.getInt("circuitCacheSize", "GENERAL", 20, 0, Integer.MAX_VALUE, "The maximum number of undos that can be used in the CAD");
+		enableTracker = config.getBoolean("enableTracker", "GENERAL", true,
+			"This setting will make the game visit this URL on startup: https://raw.githubusercontent.com/Victorious3/Integrated-Circuits/master/version.dat\n" +
+			"The connection is established over bit.ly for statistics. The data collected is publicly visible on https://bitly.com/1GIaUA6+. Bit.ly will track\n" +
+			"your country via your IP address, no other, or personal, information is gathered. I like statistics, and I would like you to keep this setting enabled,\n" +
 			"so that I get a better overview of how often my mod is used. Thanks.");
-		
+
 		config.save();
 	}
 	
