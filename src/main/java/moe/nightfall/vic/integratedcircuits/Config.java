9a5813af585b87eabae6149179d100ac51ab228a
Achati <Achati@users.noreply.github.com>
2015-04-13 17:37:19 +0200

    fixed saving of the config file (hopefully) and changed the forced restart parameters of the GuiConfig

--- src/main/java/moe/nightfall/vic/integratedcircuits/Config.java
+++ src/main/java/moe/nightfall/vic/integratedcircuits/Config.java
@@ -61,7 +61,7 @@ public class Config
 	public static void save()
 	{
 		if(!showConfirmMessage.hasChanged()) return;
-		config.save();
+			config.save();
 	}
 
 	/** Reloads the config values upon change */
@@ -69,7 +69,7 @@ public class Config
 		@SubscribeEvent
 		public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
 			if(event.modID.equals(Constants.MOD_ID))
-				save();
+				config.save();
 		}
 	}
 }
