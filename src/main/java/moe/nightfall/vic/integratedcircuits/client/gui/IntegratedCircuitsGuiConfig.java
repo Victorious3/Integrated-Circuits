342f83f7fe41dcf0eb949f87a209515bd30315f4
Achati <Achati@users.noreply.github.com>
2015-04-24 14:38:31 +0200

    improve tooltip replacing of \r characters
    added localization for titleline2 in the config gui

--- src/main/java/moe/nightfall/vic/integratedcircuits/client/gui/IntegratedCircuitsGuiConfig.java
+++ src/main/java/moe/nightfall/vic/integratedcircuits/client/gui/IntegratedCircuitsGuiConfig.java
@@ -1,9 +1,11 @@
 package moe.nightfall.vic.integratedcircuits.client.gui;
 
-import cpw.mods.fml.client.config.*;
+import cpw.mods.fml.client.config.GuiConfig;
+import cpw.mods.fml.client.config.IConfigElement;
 import moe.nightfall.vic.integratedcircuits.Config;
 import moe.nightfall.vic.integratedcircuits.Constants;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 
@@ -21,16 +23,16 @@ public class IntegratedCircuitsGuiConfig extends GuiConfig
                 Constants.MOD_ID,
                 false,
                 false,
-                getAbridgedConfigPath(Config.config.toString()));
-        titleLine2 = "Configure IntegratedCircuits here"; //TODO add localization here if possible
+                getAbridgedConfigPath(Config.config.toString()),
+                StatCollector.translateToLocal("gui.integratedcircuits.config.titleline2"));
     }
 
     public static List<IConfigElement> getConfigElements()
     {
         List<IConfigElement> configElements = new ArrayList<IConfigElement>();
 
-        ConfigElement generalCategoryElement = new ConfigElement(Config.config.getCategory(Configuration.CATEGORY_GENERAL));
-        ConfigElement partsCategoryElement = new ConfigElement(Config.config.getCategory("parts"));
+        IConfigElement generalCategoryElement = new ConfigElement(Config.config.getCategory(Configuration.CATEGORY_GENERAL));
+        IConfigElement partsCategoryElement = new ConfigElement(Config.config.getCategory("parts"));
 
         configElements.addAll(generalCategoryElement.getChildElements());
 
@@ -40,14 +42,22 @@ public class IntegratedCircuitsGuiConfig extends GuiConfig
     }
 
     private static Pattern p = Pattern.compile("\r");
+    private List lastTooltip;
 
     @Override
     public void drawToolTip(List tooltip, int x, int y)
     {
+        if(lastTooltip != null && lastTooltip == tooltip)
+        {
+            super.drawToolTip(tooltip, x, y);
+            return;
+        }
+        lastTooltip = tooltip;
+
         for(int i = 0; i < tooltip.size();i++) {
             tooltip.set(i, p.matcher((String) tooltip.get(i)).replaceAll(""));
         }
-            //tooltip.set(i, ((String)tooltip.get(i)).replace("\r", "")); }
+
         super.drawToolTip(tooltip, x, y);
     }
 }
