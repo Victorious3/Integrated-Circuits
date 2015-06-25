f607b1e362aed995477f71b10ff2bd8a0e68f4a2
Evgeniy Zhabotinsky <evg-zhabotinsky@yandex.ru>
2015-06-25 06:15:52 +0300

    Swap analog and bundled modes when converting from 0.8
    It works!

--- src/main/java/moe/nightfall/vic/integratedcircuits/cp/legacy/LegacyLoader_0_8.java
+++ src/main/java/moe/nightfall/vic/integratedcircuits/cp/legacy/LegacyLoader_0_8.java
@@ -16,4 +16,19 @@ public class LegacyLoader_0_8 extends LegacyLoader {
 	public int getVersion() {
 		return 0;
 	}
+
+	private int swapBundledAnalog(int con) {
+		// Analog is 1 (0b01), Bundled is 2 (0b10)
+		// In 0.8 it was the other way around, so swap 0b01 and 0b10
+		int mask = (con ^ (con << 1)) & 0xAA; // 0b10101010 is unsupported :(
+		mask |= mask >> 1; // Build mask with all sides to change
+		return con ^ mask; // Change them!
+	}
+
+	@Override
+	public void transformNBT(NBTTagCompound data) {
+		// Swap analog and bundled IO modes
+		NBTTagCompound props = data.getCompoundTag("properties");
+		props.setInteger("con", swapBundledAnalog(props.getInteger("con")));
+	}
 }
