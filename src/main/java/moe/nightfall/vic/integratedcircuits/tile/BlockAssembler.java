0ccbab2e7b68f6937899c2a6e1b4e4ec9ed060fc
Victorious3 <vic@nightfall.moe>
2015-12-15 17:31:12 +0100

    BlockAssembler

--- src/main/java/moe/nightfall/vic/integratedcircuits/tile/BlockAssembler.java
+++ src/main/java/moe/nightfall/vic/integratedcircuits/tile/BlockAssembler.java
@@ -2,36 +2,32 @@ package moe.nightfall.vic.integratedcircuits.tile;
 
 import moe.nightfall.vic.integratedcircuits.Constants;
 import moe.nightfall.vic.integratedcircuits.IntegratedCircuits;
-import moe.nightfall.vic.integratedcircuits.client.Resources;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IIcon;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
 public class BlockAssembler extends BlockContainer {
 	public BlockAssembler() {
 		super(Material.iron);
-		setBlockName(Constants.MOD_ID + ".assembler");
+		setUnlocalizedName(Constants.MOD_ID + ".assembler");
 		setCreativeTab(IntegratedCircuits.creativeTab);
 		setHardness(2F);
 	}
 
 	@Override
-	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7,
-			float par8, float par9) {
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side, float hitX, float hitY, float hitZ) {
 		if (!world.isRemote)
-			player.openGui(IntegratedCircuits.instance, 1, world, x, y, z);
+			player.openGui(IntegratedCircuits.instance, 1, world, pos.getX(), pos.getY(), pos.getZ());
 		return true;
 	}
 
@@ -41,70 +37,38 @@ public class BlockAssembler extends BlockContainer {
 	}
 
 	@Override
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int side, int meta) {
-		return getIcon(null, side);
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(IBlockAccess world, int x, int y, int z, int s) {
-		return getIcon((TileEntityAssembler) world.getTileEntity(x, y, z), s);
-	}
-
-	@SideOnly(Side.CLIENT)
-	private IIcon getIcon(TileEntityAssembler te, int s) {
-		int rotation = te != null ? te.rotation : 0;
-
-		// TODO Maybe... just maybe use EnumFacing's rotating here?
-		if (s == 0)
-			return Resources.ICON_ASSEMBLER_BOTTOM;
-		else if (s == 1)
-			return Resources.ICON_ASSEMBLER_TOP;
-		else if (s == 2 && rotation == 0 || s == 5 && rotation == 1 || s == 3 && rotation == 2 || s == 4
-				&& rotation == 3)
-			return te != null && te.laserHelper.isRunning ? Resources.ICON_ASSEMBLER_FRONT_ON
-					: Resources.ICON_ASSEMBLER_FRONT_OFF;
-		else if (s == 3 && rotation == 0 || s == 4 && rotation == 1 || s == 2 && rotation == 2 || s == 5
-				&& rotation == 3)
-			return Resources.ICON_ASSEMBLER_BACK;
-		else
-			return Resources.ICON_ASSEMBLER_SIDE;
-	}
-
-	@Override
-	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack stack) {
+	public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase entity, ItemStack stack) {
 		int rotation = MathHelper.floor_double((double) (entity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-		TileEntityAssembler te = (TileEntityAssembler) world.getTileEntity(x, y, z);
+		TileEntityAssembler te = (TileEntityAssembler) world.getTileEntity(pos);
 		if (te != null)
 			te.rotation = rotation;
 	}
 
 	@Override
-	public void onBlockPreDestroy(World world, int x, int y, int z, int meta) {
-		TileEntityAssembler te = (TileEntityAssembler) world.getTileEntity(x, y, z);
+	public void breakBlock(World world, BlockPos pos, IBlockState state) {
+		TileEntityAssembler te = (TileEntityAssembler) world.getTileEntity(pos);
 		te.dropContents();
 	}
 
 	@Override
-	public void onNeighborBlockChange(World world, int x, int y, int z, Block block) {
-		TileEntityAssembler te = (TileEntityAssembler) world.getTileEntity(x, y, z);
+	public void onNeighborBlockChange(World world, BlockPos pos, IBlockState state, Block block) {
+		TileEntityAssembler te = (TileEntityAssembler) world.getTileEntity(pos);
 		te.onNeighborBlockChange();
 	}
-
+	
 	@Override
-	public int isProvidingWeakPower(IBlockAccess world, int x, int y, int z, int meta) {
-		return isProvidingStrongPower(world, x, y, z, meta);
+	public int getWeakPower(IBlockAccess world, BlockPos pos, IBlockState state, EnumFacing side) {
+		return getStrongPower(world, pos, state, side);
 	}
 
 	@Override
-	public int isProvidingStrongPower(IBlockAccess world, int x, int y, int z, int meta) {
-		TileEntityAssembler te = (TileEntityAssembler) world.getTileEntity(x, y, z);
+	public int getStrongPower(IBlockAccess world, BlockPos pos, IBlockState state, EnumFacing side) {
+		TileEntityAssembler te = (TileEntityAssembler) world.getTileEntity(pos);
 		return te.isProvidingPower();
 	}
 
 	@Override
-	public boolean isSideSolid(IBlockAccess world, int x, int y, int z, EnumFacing side) {
+	public boolean isSideSolid(IBlockAccess world, BlockPos pos, EnumFacing side) {
 		return true;
 	}
 
@@ -114,21 +78,17 @@ public class BlockAssembler extends BlockContainer {
 	}
 
 	@Override
-	public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side) {
-		return side != -1;
+	public boolean canConnectRedstone(IBlockAccess world, BlockPos pos, EnumFacing side) {
+		return side != EnumFacing.DOWN;
 	}
 
 	@Override
-	public boolean rotateBlock(World world, int x, int y, int z, EnumFacing axis) {
-		return ((TileEntityAssembler) world.getTileEntity(x, y, z)).rotate();
+	public boolean rotateBlock(World world, BlockPos pos, EnumFacing axis) {
+		return ((TileEntityAssembler) world.getTileEntity(pos)).rotate();
 	}
 
 	@Override
 	public boolean isOpaqueCube() {
 		return false;
-	}
-
-	@Override
-	public void registerBlockIcons(IIconRegister p_149651_1_) {
 	}
 }
